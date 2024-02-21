1750f6242621778aa1e4393081a7d16f382e5f76
Hilde-Otieno <meghanadhiambootieno@gmail.com>
2024-02-21 07:44:16 +0300

    add file

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,31 @@
+fun main(){
+name()
+   var z= remainder(84,10)
+    println(z)
+   var x= add(15,12,8,22)
+    println(x)
+    getInterest()
+}
+fun name(){
+    var name="Hello$(Megan)"
+    name="Hello$(Megan)"
+    println(name)
+}
+fun remainder(num1:Int,num2:Int):Int{
+    var num1=84
+    var num2=10
+   var  division= num1 % num2
+    return division
+}
+fun add(a:Int,b:Int,c:Int,d:Int):Int{
+    var a=15
+    var b=12
+    var c=8
+    var d=22
+    var sum= a+b+c+d
+    return sum
+}
+fun getInterest(){
+    var interest="I am kind"
+    println(interest)
+}
